b54259b48f37867b4eacb4e7a3faa5e1b1624248
KristinaGagalova <kristina.gagalova@curtin.edu.au>
2025-06-22 15:35:14 +0000

    Deploying to gh-pages from @ KristinaGagalova/KristinaGagalova.github.io@0aef1dffe4f45e93dd65c73200517601ca7a0a4a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -497,18 +497,46 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/kgagalova.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6B%72%69%73%74%69%6E%61.%67%61%67%61%6C%6F%76%61@%63%75%72%74%69%6E.%65%64%75.%61%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/KristinaGagalova", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/kristina-gagalova", "_blank");
+        },
+      },{
+        id: 'social-mastodon',
+        title: 'Mastodon',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://genomic.social/@kgagalova", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-5975-0805", "_blank");
         },
       },{
         id: 'social-rss',
@@ -522,7 +550,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=1claeqYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
